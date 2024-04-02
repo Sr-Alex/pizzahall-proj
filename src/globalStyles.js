2eead31cd5798d5627ab38b3cc53d75c71585697
Alexandre Ricardo <alexandre.n314159@gmail.com>
2024-04-02 15:01:21 -0300

    adicionado arquivo globalStyles

---
+++ src/globalStyles.js
@@ -0,0 +1,11 @@
+export default globalStyles = {
+     colors: {
+          snowWhite: "#F9F9F9",
+          offWhite: "#F5F5F5",
+          gray: "#555555",
+          lightGray: "#CDCDCD",
+          darkPurple: "#301934",
+          orange: "#F5BE14",
+          black: "#1B1212",
+     }
+};
